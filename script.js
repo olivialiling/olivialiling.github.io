97717c86084d31a08066392e18707b2454424de2
oliveeyawang <owang165@icloud.com>
2025-06-18 16:09:22 -0400

    toggle test

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('active');
+
+    // Swap icon
+    if (sidebar.classList.contains('active')) {
+      toggleButton.textContent = '✕';
+    } else {
+      toggleButton.textContent = '☰';
+    }
+  }
   }
